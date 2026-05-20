12be8bd47adb9d5d30346202d74b6e571d12944d
LakshakarLalit <l.l12052002@gmail.com>
2026-05-20 16:00:46 +0530

    Datatypes

---
+++ 03_dataType/datatype.js
@@ -0,0 +1,24 @@
+// Number       =   1,2,3,...
+// String       =   anything you can written in dualQuote("")
+// Boolean      =   true, false
+// null         =   Empty variable, and the type of null is object
+// undefined    =   Not define anything
+// symbol
+
+
+let name = "Sofia";
+let age = 40;
+let isMarrige = true;
+let isNull = null;
+let notYet;
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isMarrige));
+console.log(typeof(isNull));
+console.log(typeof(notYet));
+
+// console.log(typeof"name");
+
+
+// Object
