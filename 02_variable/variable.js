8a0072e5115f17bd82765ea5316cc3281b25f96d
LakshakarLalit <l.l12052002@gmail.com>
2026-05-20 15:06:09 +0530

    Variable and keyword

---
+++ 02_variable/variable.js
@@ -0,0 +1,13 @@
+const accountId = 144563;
+let accountEmail = "FrozenGaming";
+var accountPassword = "425156341";
+accountCity = "Nagpur";
+
+// accountId = 222222222;
+accountEmail = "rrrrrrrrrrrrrr";
+accountPassword = "111111111111111";
+accountCity = "Sadan";
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
