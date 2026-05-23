b89d27144d1129d2206243cd21ac22d8e2131d5c
LakshakarLalit <l.l12052002@gmail.com>
2026-05-23 08:29:56 +0530

    Comparision in JS

---
+++ 05_comparision/comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2">1);     // true
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// ===      -   check equal or not and also check data type is or not
+
+console.log("2" === 2);
+console.log("2" == 2);
