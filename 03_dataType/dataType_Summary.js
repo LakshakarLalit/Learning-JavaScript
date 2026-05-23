962ba7e10a6f6e7043aaad27313170fa1762ae9a
LakshakarLalit <l.l12052002@gmail.com>
2026-05-23 19:19:38 +0530

    Data Type Summery

---
+++ 03_dataType/dataType_Summary.js
@@ -0,0 +1,37 @@
+// Data types = There are two types of data in java Script
+// 1. Primitive
+// 2. Non-Primitive / Reference
+
+// Primitive = 7 types
+
+// 1- String, 2- Number, 3- Boolean, 4- Null, 5- Undefined, 6- Symbol, 7- BigInt
+
+
+// const score = 100;              Number
+// const scoreValue = 100.3;       Number
+
+// const userEmail = false;        Boolean
+// const outSideTemp = null;       Null
+// let userEmail;                  Undefined
+// const id = Symbol('123');       Symbol
+// let name = "Frozen";            String
+// const bigNumber = 134813241433n;    BigInt
+
+
+// Reference (Non-Primitive)
+
+// 1- Array, 2- Objects, 3- Functions
+
+// Array
+// const villan = ["taatybicchu", "omphatsuaaha", "mugembo"]; 
+
+// Object
+// let myObject = {
+//     name: "lalantop",                               
+//     age: 22,
+// }
+
+// Function
+// const myFun = function(){
+//     console.log("Hello");
+// }
