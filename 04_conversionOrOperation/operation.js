06f65dc0a617d552f9ddb0c9fe5fe196742c4dd5
LakshakarLalit <l.l12052002@gmail.com>
2026-05-23 08:05:56 +0530

    Operation

---
+++ 04_conversionOrOperation/operation.js
@@ -0,0 +1,23 @@
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(3+2);
+// console.log(3-2);
+// console.log(3*2);
+// console.log(3**2);
+// console.log(3/2);
+// console.log(3%2);
+
+
+let str1 = "Hello ";
+let str2 = "Kokila";
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1"+2);     // 12
+console.log(1+"2");     // 12
+console.log("1"+2+2);   // 122
+console.log(1+2+"2");   // 32
