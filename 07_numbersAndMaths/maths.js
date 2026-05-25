eb67718bf2a1dd98ca164ce7756f15b47c221fd2
LakshakarLalit <l.l12052002@gmail.com>
2026-05-25 13:56:09 +0530

    Math

---
+++ 07_numbersAndMaths/maths.js
@@ -0,0 +1,8 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(8, 9, 6, 3));
+console.log(Math.max(8, 9, 6, 3));
