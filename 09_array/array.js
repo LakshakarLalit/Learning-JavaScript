bc49c9a818bdabdf791b4099711f2f88a5d5aae1
LakshakarLalit <l.l12052002@gmail.com>
2026-05-26 15:17:58 +0530

    Array and methods

---
+++ 09_array/array.js
@@ -0,0 +1,18 @@
+// Array
+const heros = ["sisimaaru", "ninja", "rakuun"];
+const arr2 = new Array(1, 2, 3, 4);
+
+const myArr = [0, 1, 2, 3, 4];
+console.log(myArr[3]);
+
+
+// methods
+
+myArr.push(6);
+console.log(myArr);
+myArr.pop()
+console.log(myArr);
+
+// myArr.unshift(35);
+// myArr.shift();
+
