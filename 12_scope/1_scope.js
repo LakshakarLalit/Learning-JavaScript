91ae6fab6b15564ff01ad97cae5a8d3d0a7e54d3
LakshakarLalit <l.l12052002@gmail.com>
2026-05-31 19:22:20 +0530

    Scope in function

---
+++ 12_scope/1_scope.js
@@ -0,0 +1,12 @@
+if (true){
+    let a = 10;
+    const b= 20;                        // block scope
+    var c = 30;    
+}
+let name = "neema";                     // Global scope
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+console.log(name);
+
---
+++ 12_scope/2_nestedScope.js
@@ -0,0 +1,36 @@
+function one() {
+    const username = "monti";
+
+    function two(){
+        const site = "youtube";
+        // console.log(username);
+    };
+    // console.log(site);
+
+    // two();
+};
+// one();
+
+if(true){
+    const username = "saanu";
+    if(username === "saanu"){
+        const site = " youtube";
+        // console.log(username+site);  
+    };
+    // console.log(site);
+    
+};
+// console.log(username);
+
+
+
+
+function add(num){
+    return num+1
+};
+add(9);
+
+const addtwo = function(num){
+    return num+2
+};
+addtwo(5);
