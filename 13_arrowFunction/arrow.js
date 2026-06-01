6fe680a0c8632229d311da85dba0b8bb8774664e
LakshakarLalit <l.l12052002@gmail.com>
2026-06-01 22:19:21 +0530

    Arrow Function

---
+++ 13_arrowFunction/arrow.js
@@ -0,0 +1,46 @@
+// const user = {
+//     username: "naklesh",
+//     price: 896,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username}, welcome to webiste`);
+//         // console.log(this);
+        
+//     }
+// }
+
+// // user.welcomeMessage();
+// // user.username = "kallu";
+// // user.welcomeMessage();
+
+// function rola(){
+//     // console.log(this);
+    
+// }
+
+// rola()
+
+// const ramu = function(){
+//     let username = "mahesh";
+//     console.log(this.username);    
+// }
+
+// ramu()
+
+// const ramu = () => {
+//     let username = "mahesh";
+//     console.log(this );
+    
+// }
+// ramu()
+
+//  ARROW FUNCTION
+
+const addTwo = (num1, num2) => {
+    return num1+num2
+};
+console.log(addTwo(8,9));
+
+const addTwo = (num1, num2) => num1+num2
+console.log(addTwo(8,10));
+
