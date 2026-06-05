9c846f40b235105327966d0f2ef5f2d9f5776031
LakshakarLalit <l.l12052002@gmail.com>
2026-06-05 14:53:08 +0530

    True value

---
+++ 15_control_flow/truthy.js
@@ -0,0 +1,24 @@
+const username = [];
+if(username){
+    console.log("have a user email");
+} else {
+    console.log("Have not user email");
+}
+
+
+// Falsy valuse
+
+// false, 0, -0, BigInt, 0n, "", null, undefined, NaN
+
+
+
+//Truty Valuse
+
+"0", 'false', "false", " ", [], {}, function(){}
+
+
+// if (username.length===0){
+//     console.log("Array is empty");
+// }
+
+// false == 0, false = "", 0 == ""       -       true(they all are true)
