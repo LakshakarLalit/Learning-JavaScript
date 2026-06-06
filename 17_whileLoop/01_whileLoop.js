db863a4db36d30621cd956fa9c2e6c8801ea8e58
LakshakarLalit <l.l12052002@gmail.com>
2026-06-06 19:40:15 +0530

    while loop

---
+++ 17_whileLoop/01_whileLoop.js
@@ -0,0 +1,23 @@
+// DO-WHILE LOOP
+
+// initilazation
+// while (condition) {
+//     statements;
+//     increment/decrement
+// }
+
+let i = 1;
+while(i<=10)
+{
+    // console.log(`Value of i is ${i}`);
+    i+=2;
+};
+
+let myArray = ["Tatya", "kaalicharan", "Hidimba", "Shakal"];
+
+let arr = 0;
+while(arr<myArray.length)
+{
+    console.log(`Value of ${arr} =  ${myArray[arr]}`);
+    arr++
+}
