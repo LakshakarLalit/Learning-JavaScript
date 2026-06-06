409146df6c10d2ca93f833c3e408b29c4d29929d
LakshakarLalit <l.l12052002@gmail.com>
2026-06-06 18:21:47 +0530

    Loop in array

---
+++ 16_for_loop/03_loopInArray.js
@@ -0,0 +1,6 @@
+let myArray = ["Tatya", "kaalicharan", "Hidimba", "Shakal"];
+console.log(myArray.length);
+for(let index = 0; index<myArray.length; index++){
+    const element = myArray[index];
+    console.log(element);
+}
