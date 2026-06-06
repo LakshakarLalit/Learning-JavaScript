d44da84566828c26fb6e6060387096bf3d6b7970
LakshakarLalit <l.l12052002@gmail.com>
2026-06-06 19:41:15 +0530

    Do-while

---
+++ 17_whileLoop/do_while.js
@@ -0,0 +1,14 @@
+// DO-WHILE LOOP
+
+
+// initilazation
+// do {
+    // statement    
+    // increment/decrement;
+// } while (condition);
+
+let score = 1;
+do{
+    console.log("score is ", score);
+    score++;
+}while(score<=10);
