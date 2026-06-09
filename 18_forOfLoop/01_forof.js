51c2ae5c72756472c9505978060a79a9837ec5e9
LakshakarLalit <l.l12052002@gmail.com>
2026-06-09 16:26:40 +0530

    for of loop

---
+++ 18_forOfLoop/01_forof.js
@@ -0,0 +1,38 @@
+// for (const element of object) {
+    
+// }
+
+
+const arr = [1,2,3,4,5]
+for(const num of arr){
+    // console.log(num);
+};
+
+const greetings = "Hello world!";
+for(const greet of greetings){
+    // console.log(`Each char is ${greet}`);
+}
+
+
+// MAPS
+ 
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+// console.log(map);
+
+for(const [key, value] of map){
+    console.log(key, ":-", value)
+}
+
+// const myobj = {
+//     'game1': 'PUBG',
+//     'game2': 'Mario'
+
+// }
+
+// for(const [key, value] of myobj){
+//     console.log(key, ":", value)
+// }
