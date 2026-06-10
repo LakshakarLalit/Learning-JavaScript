c5d2311e4baf8c952f8bc759efe9da4cb79ee001
LakshakarLalit <l.l12052002@gmail.com>
2026-06-10 14:41:37 +0530

    for In loop

---
+++ 19_fonInLoop/01_fonIn.js
@@ -0,0 +1,22 @@
+// for (const key in object) {
+//     if (!Object.hasOwn(object, key)) continue;
+    
+//     const element = object[key];
+    
+    
+// }
+
+
+const myobj = {
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+
+for(const key in myobj){
+    // console.log(key);
+    // console.log(myobj[key])
+    // console.log(`${key} shortcut is for ${myobj[key]}`)
+}
+
